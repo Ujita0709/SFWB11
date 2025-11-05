6fc133b8f96efc08570a110432aa573dbf49eb6c
Hiroki_ujita <hiroki_ujita@hotmail.com>
2025-11-05 11:13:53 +0900

    IndexController修正

--- src/main/java/com/example/app/controller/IndexController.java
+++ src/main/java/com/example/app/controller/IndexController.java
@@ -1,35 +1,34 @@
 package com.example.app.controller;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.app.domain.Item;
+import com.example.app.mapper.FileMapper;
+
+import lombok.RequiredArgsConstructor;
 
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 	
 	private static final String UPLOAD_DIRECTORY = "C:/Users/zdis58/gallery";
+	private final FileMapper mapper;
 	
 	@GetMapping("/")
-	public String inputFileName() {
+	public String showList(Model model) {
+		File uploadsDirectory = new File(UPLOAD_DIRECTORY);
+		List<Item> itemList = mapper.selectAll();
+		model.addAttribute("items", itemList);
+		
 		return "index";
 	}
 	
-	@PostMapping("/")
-	public String showGallery(Model model) {
-		// アップロードされているファイルのリスト(表示用)の取得
-		 File uploadsDirectory = new File(UPLOAD_DIRECTORY);
-		 File[] fileList = uploadsDirectory.listFiles();
-		 List<String> fileNames = Arrays.stream(fileList)
-		 .map(file -> file.getName()).toList();
-		 model.addAttribute("fileNames", fileNames); 
-		
-		return "redirect/upload";
-	}
+	
 
 }
